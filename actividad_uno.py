03d13dec9c750736ffcbcb2bafb286f03ea3e5df
JParadae <javieraparadae@gmail.com>
2021-11-02 11:25:57 -0300

    primera actividad en python acadmia yunus

---
+++ actividad_uno.py
@@ -0,0 +1,10 @@
+"""Aticivdad N°1: 
+Formulario en python
+"""
+#Declaro variables nombre, apellido, correo, direccion, edad, fecha_nacimiento
+nombre = input('Cuál es tu nombre? :')
+apellido = input('Cuál es tu apellido? :')
+edad = int(input('Cual es tu edad: '))
+#Imprimiendo datos del formulario
+print(nombre, apellido, edad)
+
