de90a013172518c4bfc59dc6396a04fe3f187aca
Javiera Parada <javieraparadae@gmail.com>
2021-11-05 16:02:57 -0300

    Update actividad_3.py

--- actividad_3.py
+++ actividad_3.py
@@ -1,2 +1,16 @@
-"""hola"""
-variable = "javiera"
+###Evaluando si es un numero,letra, decimal
+numero = 1
+letra = "bsdfsdf" 
+decimal = 2.5
+
+"""estoy consultando el tipo de dato es flotante?, sino imprimo el tipo de dato"""
+if type(letra) is float: 
+    print('es letra')
+else:
+    print(f'El tipo de dato es: {type(letra)}') #imprimo el tipo de dato
+
+"""estoy consultando el tipo de dato es string?, sino imprimo el tipo de dato"""
+if type(numero) is str:   
+    print('esto es un numero')  #imprimo el tipo de dato
+else: 
+    print(f'El tipo de dato es: {type(numero)}') 
