81ec53983f3460c0d5c4bc0ae3c51b284748a463
Javiera Parada <javieraparadae@gmail.com>
2021-11-06 18:32:02 -0300

    modificando el archivo para el video

--- actividad_3.py
+++ actividad_3.py
@@ -1,7 +1,9 @@
 ###Evaluando si es un numero,letra, decimal
 numero = 1
-letra = "bsdfsdf" 
+letra = "a" 
 decimal = 2.5
+#Agregar otras variables que contengan otros type of data
+variable_booleana = True
 
 """estoy consultando el tipo de dato es flotante?, sino imprimo el tipo de dato"""
 if type(letra) is float: 
@@ -14,3 +16,10 @@ if type(numero) is str:
     print('esto es un numero')  #imprimo el tipo de dato
 else: 
     print(f'El tipo de dato es: {type(numero)}') 
+
+
+"""Ejemplo de evaluacion de tipo de dato booleano"""
+if type(variable_booleana) is int:
+    print("La variable es booleana")
+else:
+    print(f'La variable de nombre variable_booleana es de tipo:  {type(variable_booleana)} ')    
